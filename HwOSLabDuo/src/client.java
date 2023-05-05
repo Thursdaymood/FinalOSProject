7db01cc9f645aecedbf5cb8fcaf357167241c440
Thursdaymood <thedeliveredream@gmail.com>
2023-05-05 18:48:11 +0700

    Sample connection

--- HwOSLabDuo/src/client.java
+++ HwOSLabDuo/src/client.java
@@ -5,55 +5,72 @@ import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
+
+
 public class client {
-	private static int port = 0;
-	private static String ip= "";
-	private static Scanner scan = new Scanner(System.in);
+	
+	public static int port = 0;
+	public static String ipUser = "";
 	
 	// Constructor
-	client(){
+	client(int port, String ipUser){
+		this.port = port;
+		this.ipUser = ipUser ;
 		
-	}
-
-	public static void main(String[] args) {
-		
-		ClientRunnable tmp = new ClientRunnable(port);
-		
-		
-
+		//Create thread to take responsibility about client
+		ClientRunnable task = new ClientRunnable(ipUser, port);
+		Thread threadClient = new Thread(task);
+		threadClient.start();		
 	}
 	
-	// get method
-	public String getIP() {
-		return this.ip;
-	}
-	public int getPort() {
-		return this.port;
-	}
-	// set method
-	public void setIP(String user) {
-		this.ip = user;
-	}
-	public void setPort(int user) {
-		this.port = user;
-	}
 	
+	
+// The the end of client class	
 }
 
+// About thread
 class ClientRunnable implements Runnable {
-
+	
+	// Variables
 	private static int port = 0;
 	private static String ip= "";
 	private static Scanner scan = new Scanner(System.in);
+	//variable network
+	Socket socket = null;
+	DataInputStream input = null;
+	DataOutputStream output = null;
 
 	// Constructor
-	ClientRunnable(int portUser) {
+	ClientRunnable(String ipUser, int portUser) {
 
 		this.port = portUser;
+		this.ip = ipUser;
 	}
 
 	@Override
 	public void run() {
+		
+		try {
+			// 1) Connect the server in the same socket by choosing ip of server and port number
+			socket = new Socket(ip, port);
+			output = new DataOutputStream(socket.getOutputStream());
+			
+			// 2) Communicate
+			
+			// test connection
+			output.writeUTF("\tHello!!");
+			
+			// 3) Close
+			output.close();
+			socket.close();
+			
+			
+
+			
+			
+		}catch(Exception error) {
+			System.err.println(error.getMessage());
+		}
 
 	}
 }
