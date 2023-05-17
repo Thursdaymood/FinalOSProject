884b532726da3b3ead667906a9787311b2e9c85f
Mindvezar <nichapa.por@dome.tu.ac.th>
2023-05-17 13:50:31 +0700

    Update GUI

--- HwOSLabDuo/src/game.java
+++ HwOSLabDuo/src/game.java
@@ -1,78 +1,208 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class game extends JFrame {
-    private static final int WIDTH = 800;
-    private static final int HEIGHT = 500;
-    static final int margin = 15;
+
+    //store wordChallenge exmport form roomserver
+    private String[] wordChallenge = new String[] {"art","acer","action","acid","alot"};
+
+    private roomServer server;
+    private String word ; //แก้การเข้าถึงกำหนดindex
+    private int lifeOfPlayer1 =3 , lifeOfPlayer2 = 3;
+    private JLabel hiddeWordsLabel,play1,play2,round,displayScore;
+
+    private int turn = 1;
+    private final int WIDTH = 800;
+    private final int HEIGHT = 500;
+    static int margin = 30;
     static final int size_heart = 30;
-    static Color white = Color.WHITE;
-    public static void createGUI() {
-        
-    }
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("HANG_MAN");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(WIDTH, HEIGHT);
+    static Color TEXT_COLOR = Color.WHITE;
+    static Color BACKGROUND = Color.GRAY;
+    static Color PRIMARY_COLOR = Color.WHITE;
+    static Color SECONDARY_COLOR = Color.decode("#FCA311");
+    private Font customFont;
+    private JButton[] letterButtons;
 
-        //line
-        JPanel panel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                Graphics2D line = (Graphics2D) g;
-
-                line.setColor(white);
-                line.drawLine(200, getHeight()/2+100, 900, getHeight()/2+100);
-
-                line.setColor(white);
-                line.drawLine(200, 0, 200, 700);
-                
+    public static String hiddeWords(String word){
+        String hiddenWord = "";
+        for(int i = 0; i < word.length(); i++){
+            if(!(word.charAt(i) == ' ')){
+                hiddenWord += "_ ";
+            }else{
+                hiddenWord += " ";
             }
-        };
-
-        panel.setBackground(Color.BLACK);
-        frame.add(panel);
-        //PLAY1
-        JLabel play1 = new JLabel("PLAY1");
-        play1.setBounds(15, 30, 100, 30);
-        play1.setFont(new Font("Arial", Font.BOLD, 18));
-        play1.setForeground(Color.WHITE);
-
-        //heart
-        int pos_x = margin;
-        for (int i = 0; i < 3; i++) {
-            ImageIcon image = new ImageIcon("resc/heart.png");
-            Image smallImage = image.getImage().getScaledInstance(size_heart, size_heart, Image.SCALE_SMOOTH);
-            ImageIcon smallIcon = new ImageIcon(smallImage);
-            JLabel heart = new JLabel(smallIcon);
-            heart.setBounds(pos_x, 65, size_heart, size_heart);
-            panel.add(heart);
-
-            pos_x += size_heart+10;
         }
         
+        return hiddenWord;
 
-        //score
-        JLabel score1 = new JLabel("Score:");
-        score1.setForeground(Color.WHITE);
-        score1.setBounds(15, 100, 1000, 30);
-        score1.setFont(new Font("Arial", Font.BOLD, 18));
+    }
+
+    private Font createFont(String path) {
+		Font customFont = null;
+		
+		try {
+			customFont = Font.createFont(Font.TRUETYPE_FONT, new File(path));
+		} catch (IOException | FontFormatException e) {
+			e.printStackTrace();
+		}
+
+		return customFont;
+
+	}
+
+    private void createHeart(int life, int player){
+        int pos_x = margin;
+         for (int i = 0; i < life; i++) {
+             ImageIcon image = new ImageIcon("HwOSLabDuo/resc/heart.png");
+             Image smallImage = image.getImage().getScaledInstance(size_heart, size_heart, Image.SCALE_SMOOTH);
+             ImageIcon smallIcon = new ImageIcon(smallImage);
+             JLabel heart = new JLabel(smallIcon);
+             heart.setBounds(pos_x, 65 + ((player-1)*(HEIGHT/2-30)), size_heart, size_heart);
+             getContentPane().add(heart);
+
+             pos_x += size_heart+10;
+         }
+         System.out.println(margin);
+
+    }
+
+    private void displayScore(int height){
+        displayScore = new JLabel("SCORE: 10 ");
+        displayScore.setFont(customFont.deriveFont(20f));
+        displayScore.setBounds(margin, height , 120, 30);
+        displayScore.setForeground(TEXT_COLOR);
+        getContentPane().add(displayScore);
+    }
+
+    public game(){
+        super("Hangman Game (Java Ed.)");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        //setResizable(false);
+        getContentPane().setBackground(BACKGROUND);
+
+        // init vars
+        server = new roomServer();
+        letterButtons = new JButton[26];
+        customFont = createFont("HwOSLabDuo/resc/Raleway-SemiBold.ttf");
+        word = wordChallenge[turn];
+        //wordChallenge = server.word_random();
+
+        // ImageIcon imageIcon = new ImageIcon("HwOSLabDuo/resc/bgPlay.jpg");
+        // Image image = imageIcon.getImage();
+        // Image scaledImage = image.getScaledInstance(WIDTH, HEIGHT, Image.SCALE_SMOOTH);
+        // ImageIcon scaledImageIcon = new ImageIcon(scaledImage);
+        // JLabel background = new JLabel(scaledImageIcon);
+        // background.setBounds(0, 0, WIDTH, HEIGHT);
+        // add(background);
+
+        addGuiComponents();
+        ++ turn ;
+    }
+    private void addGuiComponents(){
+        //display
+        play1 = new JLabel("PLAYER 1");
+        play1.setBounds(margin, 30, 120, 30);
+        play1.setFont(customFont.deriveFont(25f));
+        play1.setForeground(TEXT_COLOR);
+        createHeart(lifeOfPlayer1,1);
+        displayScore(100);
+
+        play2 = new JLabel("PLAYER 2");
+        play2.setBounds(margin, HEIGHT/2, 120, 30);
+        play2.setFont(customFont.deriveFont(25f));
+        play2.setForeground(TEXT_COLOR);
+        createHeart(lifeOfPlayer2,2);
+        displayScore(HEIGHT/2+70);
+
+        round = new JLabel("TURN: "+ (turn+1) + " / 5");
+        round.setForeground(TEXT_COLOR);
+        round.setBounds(WIDTH - 150, 30, 150, 30);
+        round.setFont(customFont.deriveFont(20f));
+
+        //hidden word
+        hiddeWordsLabel = new JLabel(hiddeWords(word));
+        hiddeWordsLabel.setFont(customFont.deriveFont(60f));
+        hiddeWordsLabel.setForeground(TEXT_COLOR);
+        hiddeWordsLabel.setBounds(WIDTH/2-hiddeWordsLabel.getWidth()/2,(HEIGHT-160)/2,WIDTH-190,100);
+        //hiddeWordsLabel.setBounds(200+(WIDTH-200)/2-70,HEIGHT-300,WIDTH-190,100);
+ 
+        //layout
+        GridLayout gridLayout = new GridLayout(3,9);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBounds( 190,HEIGHT-190,WIDTH-197,150);
+        buttonPanel.setLayout(gridLayout);
+        buttonPanel.setBorder(null);
+
+        // create the letter buttons
+        for(char c = 'A'; c <= 'Z'; c++){
+            JButton button = new JButton(Character.toString(c));
+            button.setBackground(BACKGROUND);
+            button.setFont(customFont.deriveFont(15f));
+            button.setForeground(BACKGROUND);
+            button.setOpaque(true);
+            //button.addActionListener(this);
+
+            // using ASCII values to caluclate the current index
+            int currentIndex = c - 'A';
+
+            letterButtons[currentIndex] = button;
+            buttonPanel.add(letterButtons[currentIndex]);
+        }
+
+        // // reset button
+        // JButton resetButton = new JButton("Reset");
+        // resetButton.setFont(customFont.deriveFont(15f));
+        // resetButton.setForeground(BACKGROUND);
+        // resetButton.setBackground(BACKGROUND);
+        // resetButton.setOpaque(true);
+        // //resetButton.addActionListener(this);
+        // buttonPanel.add(resetButton);
+
+        // quit button
+        JButton giveUpButton = new JButton("Give Up");
+        giveUpButton.setFont(customFont.deriveFont(13f));
+        giveUpButton.setForeground(BACKGROUND);
+        giveUpButton.setBackground(BACKGROUND);
+        giveUpButton.setOpaque(true);
+        //giveUpButton.addActionListener(this);
+        buttonPanel.add(giveUpButton);
 
         
-        panel.setLayout(null);
-        panel.add(play1);
-        panel.add(score1);
+        getContentPane().add(play1);
+        getContentPane().add(play2);
+        getContentPane().add(round);
+        getContentPane().add(buttonPanel);
+        getContentPane().add(hiddeWordsLabel);
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+        String command = e.getActionCommand();
+
+        // disable button
+        JButton button = (JButton) e.getSource();
+        button.setEnabled(false);
+
+        if (wordChallenge[1].contains(command)) {
+            button.setBackground(Color.GREEN);
+        }
+        else{
+            button.setBackground(Color.RED);
+        }
         
-
-        //turn
-        JLabel turn = new JLabel("TURN");
-        turn.setForeground(Color.WHITE);
-        turn.setBounds(WIDTH - 150, 30, 100, 30);
-        turn.setFont(new Font("Arial", Font.BOLD, 18));
-        panel.add(turn);
-
-        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new game().setVisible(true);
     }
 
 }
